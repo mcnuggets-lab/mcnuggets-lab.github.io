f10aa67af0ea04b01e44f8696a3a57760a8445ba
Kit-Ho Mak <mcnuggets23@gmail.com>
2019-11-18 23:18:08 +0800

    build tictactoe

---
+++ tictactoe/precache-manifest.d5e5816013545cd7ee3c8b26bbfce3ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3092f7e8bc8cb53d6fb466626608c524",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "637bd01b2528a95c08b9",
+    "url": "/tictactoe/static/css/main.7d0cf010.chunk.css"
+  },
+  {
+    "revision": "acf2a7ff4acf926c77f2",
+    "url": "/tictactoe/static/js/2.c44deec4.chunk.js"
+  },
+  {
+    "revision": "637bd01b2528a95c08b9",
+    "url": "/tictactoe/static/js/main.5720b21f.chunk.js"
+  },
+  {
+    "revision": "d49b0fb29a7b15b6fd26",
+    "url": "/tictactoe/static/js/runtime-main.321013cd.js"
+  }
+]);
