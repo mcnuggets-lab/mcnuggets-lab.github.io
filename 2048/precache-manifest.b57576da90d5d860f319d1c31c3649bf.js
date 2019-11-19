8ff48d1f500c6d08c7b389f554aba63497cde8c2
Kit-Ho Mak <mcnuggets23@gmail.com>
2019-11-19 21:56:44 +0800

    Build 2048

---
+++ 2048/precache-manifest.b57576da90d5d860f319d1c31c3649bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db10b31bca92a34015f323018b7f2f8",
+    "url": "/2048/index.html"
+  },
+  {
+    "revision": "4820c6ef5205575d01fe",
+    "url": "/2048/static/css/main.4ce6a07c.chunk.css"
+  },
+  {
+    "revision": "9a1dbb9014b5782d448f",
+    "url": "/2048/static/js/2.3d031692.chunk.js"
+  },
+  {
+    "revision": "4820c6ef5205575d01fe",
+    "url": "/2048/static/js/main.92529cb6.chunk.js"
+  },
+  {
+    "revision": "4abee17fdb9e84a981be",
+    "url": "/2048/static/js/runtime-main.188feadd.js"
+  }
+]);
